4759a6bd28461fbe02ff7ef9f285e4fda95a2540
faceves <faceves@ucsd.edu>
2021-04-19 15:52:39 -0700

    Finished Student case class/companion object

--- project0/src/main/scala/tutorapp/Student.scala
+++ project0/src/main/scala/tutorapp/Student.scala
@@ -1,6 +1,20 @@
 package tutorapp
 
+import java.sql.ResultSet
+
+case class Student(fname: String, lname: String, studentID: Int, classID: Int, classGrade: Char){}
+
 class Student() {
   /**Transforms the result set information into objects. **/
-  def objectifyResultSet():Unit 
+  def objectifyResultSet(rs : ResultSet): Student ={
+    //use the generated case class apply that the companion object also gets to create an instance
+    val studentObj =  apply(
+                        rs.getString(1),
+                        rs.getString(2),
+                        rs.getInt(3),
+                        rs.getInt(4),
+                        rs.getCharacterStream(5)
+                      )
+    studentObj
+  }
 }
