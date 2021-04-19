624d31b9660718c23ea5138b8c677fcfb8cebc2b
faceves <faceves@ucsd.edu>
2021-04-19 15:52:47 -0700

    Finished Student case class/companion object

--- project0/src/main/scala/tutorapp/Student.scala
+++ project0/src/main/scala/tutorapp/Student.scala
@@ -4,7 +4,7 @@ import java.sql.ResultSet
 
 case class Student(fname: String, lname: String, studentID: Int, classID: Int, classGrade: Char){}
 
-class Student() {
+object Student{
   /**Transforms the result set information into objects. **/
   def objectifyResultSet(rs : ResultSet): Student ={
     //use the generated case class apply that the companion object also gets to create an instance
